52eb756c543fb6c4cea2192c9bf772d66d1f2210
med-hmama <med.hmama01@gmail.com>
2024-05-05 16:36:28 +0100

    done

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        firstName: username,
+      },
+      { headers: { "private-key": "//private key" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (error) {
+    return res.status(error.response.status).json(error.response.data);
+  }
+});
+
+app.listen(3001);
